6d4ff9f099eb57dc5452c126de213f0e0eb96039
Chris Rodriguez <github@chrisrodriguez.me>
2016-08-02 09:46:50 -0400

    Updating page object for data download

--- cms/static/js/views/xblock_string_field_editor.js
+++ cms/static/js/views/xblock_string_field_editor.js
@@ -6,7 +6,7 @@
  * be removed and the input hidden again.
  */
 define(['js/views/baseview', 'js/views/utils/xblock_utils', 'edx-ui-toolkit/js/utils/html-utils'],
-    function (BaseView, XBlockViewUtils, HtmlUtils){
+    function(BaseView, XBlockViewUtils, HtmlUtils) {
         'use strict';
         var XBlockStringFieldEditor = BaseView.extend({
             events: {
