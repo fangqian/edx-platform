664c9be2359dc3d72f650bf90a0aa03ba0e38182
Eric Fischer <efischer@edx.org>
2016-08-02 11:06:29 -0400

    data model initial migration

---
+++ lms/djangoapps/grades/migrations/0001_initial.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import coursewarehistoryextended.fields
+import django.utils.timezone
+import model_utils.fields
+import xmodule_django.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='PersistentSubsectionGradeModel',
+            fields=[
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('id', coursewarehistoryextended.fields.UnsignedBigIntAutoField(serialize=False, primary_key=True)),
+                ('subtree_edited_date', models.DateTimeField(verbose_name=b'last content edit timestamp')),
+                ('user_id', models.CharField(max_length=255)),
+                ('earned_all', models.IntegerField()),
+                ('possible_all', models.IntegerField()),
+                ('earned_graded', models.IntegerField()),
+                ('possible_graded', models.IntegerField()),
+                ('course_id', xmodule_django.models.CourseKeyField(max_length=255)),
+                ('usage_key', xmodule_django.models.UsageKeyField(max_length=255)),
+                ('course_version', models.CharField(max_length=255, verbose_name=b'guid of latest course version')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='VisibleBlocksModel',
+            fields=[
+                ('_blocks_json', models.TextField(db_column=b'blocks_json')),
+                ('hashed', models.CharField(max_length=32, serialize=False, primary_key=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='persistentsubsectiongrademodel',
+            name='visible_blocks',
+            field=models.ForeignKey(to='grades.VisibleBlocksModel'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='persistentsubsectiongrademodel',
+            unique_together=set([('user_id', 'usage_key')]),
+        ),
+    ]
